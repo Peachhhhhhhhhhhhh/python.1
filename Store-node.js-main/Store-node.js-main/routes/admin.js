7698159cd9143875a13a9654e3f9ef6c3fcb8ba4
Peachhhhhhhhhhhhh <144330517+Peachhhhhhhhhhhhh@users.noreply.github.com>
2024-04-04 09:33:59 +0700

    .

---
+++ Store-node.js-main/Store-node.js-main/routes/admin.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+const express = require("express");
+
+const router = express.Router();
+
+const rootDir = require("../util/path");
+
+router.get("/add-product", (req, res, next) => {
+  // res.sendFile(path.join(__dirname, "../views/add-product.html"));
+  // res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
+});
+
+router.use("/add-product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+module.exports = router;
